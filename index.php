97d740edf17a2a91ccbf966f22231b3ee0937fdd
Manuel Blanco <blanco.manuel.sebastian@gmail.com>
2021-06-23 18:45:53 -0300

    Se ingresa etiqueta placeholder en html

--- index.php
+++ index.php
@@ -32,19 +32,19 @@
         <form action="save_task.php" method="POST">
 
         <p>Nombre</p>
-        <input type="text" id="nombre" name="nombre" required minlength="4" maxlength="100" size="50">
+        <input type="text" id="nombre" name="nombre" required minlength="4" maxlength="100" size="50" placeholder="Ingrese su nombre">
         <br>
 
         <p>Apellido</p>
-        <input type="text" id="apellido" name="apellido" required minlength="4" maxlength="100" size="50">
+        <input type="text" id="apellido" name="apellido" required minlength="4" maxlength="100" size="50" placeholder="Ingrese su Apellido">
         <br>
 
         <p>Edad</p>
-        <input type="number" id="edad" name="edad" required minlength="1" maxlength="3" size="50">
+        <input type="number" id="edad" name="edad" required minlength="1" maxlength="3" size="60" placeholder="Ingrese por favor su edad">
         <br>
 
         <p>Telefono</p>
-        <input type="number" id="tel" name="tel" required minlength="8" maxlength="15" size="50">
+        <input type="number" id="tel" name="tel" required minlength="8" maxlength="15" size="60" placeholder="Ingrese por favor un telefono">
         <br>
         
         <p>Indique su genero</p>
